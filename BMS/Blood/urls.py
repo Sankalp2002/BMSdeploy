99121082e257e442bbc14b85e22dcb95e5e8cd92
Sankalp <sankalprajendran@gmail.com>
2021-04-18 01:17:48 +0530

    update

--- BMS/Blood/urls.py
+++ BMS/Blood/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from Blood import views
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 app_name='Blood'
 
 urlpatterns=[
     #path('Home/',views.home,name='home'),
     path('Login/',views.adminlogin,name='adminlogin'),
+    #path('Login/', LoginView.as_view(template_name='Blood/adminlogin.html'), name='adminlogin'),
     path('Panel/',views.adminpanel,name='adminpanel'),
     path('Panel/Doctor/',views.adminpaneldoctor,name='adminpaneldoctor'),
     path('Panel/Donations/',views.adminpaneldonations,name='adminpaneldonations'),
--- BMS/Doctor/urls.py
+++ BMS/Doctor/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from Doctor import views
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 app_name='Doctor'
 
 urlpatterns=[
     path('Login/',views.doclogin,name='doclogin'),
+    #path('Login/', LoginView.as_view(template_name='Doctor/doctorlogin.html'), name='doclogin'),
     path('Register/',views.docregister,name='docregister'),
     path('Panel/',views.docpanel,name='docpanel'),
     path('Panel/NewRequest',views.docpanelrequest,name='docpanelrequest'),
