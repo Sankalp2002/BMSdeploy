006eb675aea792e947991e882356b53923de84ec
Sankalp <sankalprajendran@gmail.com>
2021-04-24 22:54:32 +0530

    update

--- BMS/Donor/forms.py
+++ BMS/Donor/forms.py
@@ -5,4 +5,4 @@ from Donor.models import Donor
 class NewDonorForm(forms.ModelForm):
     class Meta:
         model=Donor
-        fields= ('name','address','phone','email','age','sex','bloodType')
+        fields= ('doctorId','name','address','phone','email','age','sex','bloodType')
