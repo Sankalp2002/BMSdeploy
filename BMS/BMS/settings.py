29bb861831fc6b17e45077888574346a0a9a92bb
Sankalp Rajendran <53993946+Sankalp2002@users.noreply.github.com>
2021-07-01 05:51:16 +0530

    Update settings.py

--- BMS/BMS/settings.py
+++ BMS/BMS/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'BMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bms',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'dbmsuser',
-        'PASSWORD': 'dbmspassword',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
